47d8318bc0640be8e9887a1e77b8556e29eff265
trishapranati123 <70008287+trishapranati123@users.noreply.github.com>
2020-08-29 13:58:56 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,8 +6,10 @@ function setup() {
   speed=random(223,321);
   weight=random(30,52);
   bullet=createSprite(50,200,50,50);
+  bullet.velocityX=speed;
+  bullet.shapeColor=255;
   wall=createSprite(1200,200,thickness,height/2); 
-  wall.shapeColor=color(80,80,80) ;
+  //wall.shapeColor=color(80,80,80) ;
 }
 function hasCollided(lbullet,lwall)
 {
@@ -23,7 +25,7 @@ function hasCollided(lbullet,lwall)
 
 function draw() {
   background(255,255,255);  
-  bullet.velocityX=speed;
+  
   if(hasCollided(bullet,wall))
   {
       bullet.velocityX=0;
